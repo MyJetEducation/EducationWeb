1b64d011612086bf6df6b3f57031ae67e98b6e45
Ruslan Goroshko <rusgoroshko@gmail.com>
2022-04-04 15:46:36 +0300

    save

---
+++ src/components/AchieventItem.tsx
@@ -0,0 +1,369 @@
+import React from 'react';
+
+import starter from './assets/icon/achievements/motivation.png';
+import ignition from './assets/icon/achievements/achievement.png';
+import voila from './assets/icon/achievements/success.png';
+import welcome from './assets/icon/achievements/success.png';
+import firstTouch from './assets/icon/achievements/success.png';
+import complaisance from './assets/icon/achievements/certificate.png';
+import Eyescatter from './assets/icon/achievements/achieving-goals.png';
+import pjFry from './assets/icon/achievements/diploma.png';
+import soEasy from './assets/icon/achievements/money.png';
+import habitant from './assets/icon/achievements/medal.png';
+import greenArrow from './assets/icon/achievements/bow-and-arrow.png';
+import nowIKnow from './assets/icon/achievements/nowIKnow.png';
+import takeYourTime from './assets/icon/achievements/wreath.png';
+import ALongWay from './assets/icon/achievements/wreath.png';
+import iHaveSeenThis from './assets/icon/achievements/seen.png';
+import bender from './assets/icon/achievements/learning.png';
+import theSeeker from './assets/icon/achievements/thumbs-up.png';
+import theGreatChanger from './assets/icon/achievements/creation.png';
+import weGotCompany from './assets/icon/achievements/competitor.png';
+import companion from './assets/icon/achievements/companion.png';
+import newBeginning from './assets/icon/achievements/newBeginning.png';
+import badLuck from './assets/icon/achievements/badLuck.png';
+import unstoppable from './assets/icon/achievements/badLuck.png';
+import paradox from './assets/icon/achievements/paradox.png';
+import spender from './assets/icon/achievements/spender.png';
+import trinity from './assets/icon/achievements/trinity.png';
+import bullseye from './assets/icon/achievements/bullseye.png';
+import insister from './assets/icon/achievements/insister.png';
+import perfectTiming from './assets/icon/achievements/insister.png';
+import doubleQuick from './assets/icon/achievements/doubleQuick.png';
+import rareCollector from './assets/icon/achievements/rareCollector.png';
+import flash from './assets/icon/achievements/flash.png';
+import youCantHide from './assets/icon/achievements/flash.png';
+import faceTime from './assets/icon/achievements/faceTime.png';
+import theHabitMaster from './assets/icon/achievements/theHabitMaster.png';
+import checkMe from './assets/icon/achievements/checkMe.png';
+import superRareCollector from './assets/icon/achievements/superRareCollector.png';
+import allTheKingsMen from './assets/icon/achievements/allTheKingsMen.png';
+import roundTheWorld from './assets/icon/achievements/roundTheWorld.png';
+import dayByDay from './assets/icon/achievements/dayByDay.png';
+import inStore from './assets/icon/achievements/inStore.png';
+import ultraRareCollector from './assets/icon/achievements/ultraRareCollector.png';
+import curious from './assets/icon/achievements/curious.png';
+import taDam from './assets/icon/achievements/taDam.png';
+import myPride from './assets/icon/achievements/myPride.png';
+import split from './assets/icon/achievements/split.png';
+import dwarf from './assets/icon/achievements/dwarf.png';
+import stability from './assets/icon/achievements/stability.png';
+import notSoHard from './assets/icon/achievements/notSoHard.png';
+
+
+
+export const ACHIEVEMENTS = [
+  {
+    id: 1,
+    type: "standard",
+    name: "Starter",
+    icon: starter,
+    description: "закончил профилирование"
+  },
+  {
+    id: 2,
+    type: "standard",
+    name: "Ignition",
+    icon: ignition,
+    description: "начал первый урок"
+  },
+  {
+    id: 3,
+    type: "standard",
+    name: "Voila",
+    icon: voila,
+    description: "закончил последний урок по первой дисциплине"
+  },
+  {
+    id: 4,
+    type: "standard",
+    name: "Welcome",
+    icon: welcome,
+    description: "впервые пригласил друга"
+  },
+  {
+    id: 5,
+    type: "standard",
+    name: "Complaisance",
+    icon: complaisance,
+  },
+  {
+    id: 6,
+    type: "standard",
+    name: "First touch",
+    icon: firstTouch,
+  },
+  {
+    id: 7,
+    type: "standard",
+    name: "Eyescatter",
+    icon: Eyescatter,
+  },
+  {
+    id: 8,
+    type: "standard",
+    name: "P.J.Fry",
+    icon: pjFry,
+  },
+  {
+    id: 9,
+    type: "standard",
+    name: "So easy..",
+    icon: soEasy,
+  },
+  {
+    id: 10,
+    type: "standard",
+    name: "Habitant",
+    icon: habitant,
+  },
+  {
+    id: 11,
+    type: "standard",
+    name: "Green Arrow",
+    icon: greenArrow,
+  },
+  {
+    id: 12,
+    type: "rare",
+    name: "Now I know",
+    icon: nowIKnow,
+  },
+  {
+    id: 13,
+    type: "rare",
+    name: "Take your time",
+    icon: takeYourTime,
+  },
+  {
+    id: 14,
+    type: "rare",
+    name: "A long way",
+    icon: ALongWay,
+  },
+  {
+    id: 15,
+    type: "rare",
+    name: "I've seen this",
+    icon: iHaveSeenThis,
+  },
+  {
+    id: 16,
+    type: "rare",
+    name: "Bender",
+    icon: bender,
+  },
+  {
+    id: 17,
+    type: "rare",
+    name: "The Seeker",
+    icon: theSeeker,
+  },
+  {
+    id: 18,
+    type: "rare",
+    name: "The great changer",
+    icon: theGreatChanger,
+  },
+  {
+    id: 19,
+    type: "rare",
+    name: "We got company",
+    icon: weGotCompany,
+  },
+  {
+    id: 20,
+    type: "rare",
+    name: "Companion",
+    icon: companion,
+  },
+  {
+    id: 21,
+    type: "rare",
+    name: "New Beginning",
+    icon: newBeginning,
+  },
+  {
+    id: 22,
+    type: "rare",
+    name: "Bad Luck",
+    icon: badLuck,
+  },
+  {
+    id: 23,
+    type: "rare",
+    name: "Unstoppable",
+    icon: unstoppable,
+  },
+  {
+    id: 24,
+    type: "rare",
+    name: "Paradox",
+    icon: paradox,
+  },
+  {
+    id: 25,
+    type: "rare",
+    name: "Spender",
+    icon: spender,
+  },
+  {
+    id: 26,
+    type: "rare",
+    name: "Trinity",
+    icon: trinity,
+  },
+  {
+    id: 27,
+    type: "superRare",
+    name: "Bullseye",
+    icon: bullseye,
+  },
+  {
+    id: 28,
+    type: "superRare",
+    name: "Insister",
+    icon: insister,
+  },
+  {
+    id: 29,
+    type: "superRare",
+    name: "Perfect timing",
+    icon: perfectTiming,
+  },
+  {
+    id: 30,
+    type: "superRare",
+    name: "Double Quick",
+    icon: doubleQuick,
+  },
+  {
+    id: 31,
+    type: "superRare",
+    name: "Rare collector",
+    icon: rareCollector,
+  },
+  {
+    id: 32,
+    type: "superRare",
+    name: "Flash",
+    icon: flash,
+  },
+  {
+    id: 33,
+    type: "superRare",
+    name: "You can't hide",
+    icon: youCantHide,
+  },
+  {
+    id: 34,
+    type: "superRare",
+    name: "FaceTime",
+    icon: faceTime,
+  },
+  {
+    id: 35,
+    type: "superRare",
+    name: "The habit master",
+    icon: theHabitMaster,
+  },
+  {
+    id: 36,
+    type: "ultraRare",
+    name: "Check me",
+    icon: checkMe,
+  },
+  {
+    id: 37,
+    type: "ultraRare",
+    name: "Super Rare collector",
+    icon: superRareCollector
+  },
+  {
+    id: 38,
+    type: "ultraRare",
+    name: "All the king's men",
+    icon: allTheKingsMen,
+  },
+  {
+    id: 39,
+    type: "ultraRare",
+    name: "Round-the-world",
+    icon: roundTheWorld,
+  },
+  {
+    id: 40,
+    type: "ultraRare",
+    name: "Day by day",
+    icon: dayByDay,
+  },
+  {
+    id: 41,
+    type: "ultraRare",
+    name: "In store",
+    icon: inStore,
+  },
+  {
+    id: 42,
+    type: "unique",
+    name: "Ultra rare collector",
+    icon: ultraRareCollector,
+  },
+  {
+    id: 43,
+    type: "unique",
+    name: "Curious",
+    icon: curious,
+  },
+  {
+    id: 44,
+    type: "unique",
+    name: "Ta-Dam!",
+    icon: taDam,
+  },
+  {
+    id: 45,
+    type: "unique",
+    name: "My pride",
+    icon: myPride,
+  },
+  {
+    id: 46,
+    type: "unique",
+    name: "Split",
+    icon: split,
+  },
+  {
+    id: 47,
+    type: "superRare",
+    name: "Dwarf",
+    icon: dwarf,
+  },
+  {
+    id: 48,
+    type: "superRare",
+    name: "Stability",
+    icon: stability,
+  },
+  {
+    id: 49,
+    type: "unique",
+    name: "Not so hard",
+    icon: notSoHard,
+  }
+]
+
+interface Props {
+
+}
+
+
+const AchieventItem = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default AchieventItem;
